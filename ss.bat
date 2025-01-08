986612b4b59f29ddf945e3a41448ad03ce016fc9
Flashznns <luizxg012@gmail.com>
2025-01-08 01:33:00 -0300

    Add files via upload

---
+++ ss.bat
@@ -0,0 +1,10 @@
+@echo off
+cls
+@echo.
+@echo    ***********************************************
+@echo    ***                                         ***
+@echo    ***  FAVOR VAMOS FICAR RICO SIM. OBRIGADO!  ***
+@echo    ***                                         ***
+@echo    ***********************************************
+@echo.
+pause
